0fc826d0a7cdd406b269a448aae3e570d4ea9b92
Nerbet <60988912+Nerbet@users.noreply.github.com>
2020-03-23 00:21:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 exports.min = function min (array) 
 {
+    if(array == undefined || array == [])
+    {
+        return 0;
+    }
     var Min = 0;
     Min = array[0];
-    if(array == [])
-    {return 0;}
-    else
-    {
     for(var i = 1; i<array.length; i++) 
     {
         if(array[i]<Min)
@@ -16,14 +16,13 @@ exports.min = function min (array)
     }
     alert(Min);
     return Min;
-    }
 }
 exports.max = function max (array) {
-    var Max = 0;
-    if(array == [])
-    {return 0;}
-    else
+        if(array == undefined || array == [])
     {
+        return 0;
+    }
+    var Max = 0;
     for(var i = 0; i<array.length; i++)
     {
         if(array[i]>array[i+1] && array[i] > Max)
@@ -32,19 +31,17 @@ exports.max = function max (array) {
         }
     }
     return Max;
-    }
   }
   
 exports.avg = function avg (array) {
-    if(array == [])
-    {return 0;}
-    else
+        if(array == undefined || array == [])
     {
+        return 0;
+    }
     var sum = 0;
     for(i=0;i<array.length;i++){
         sum = array[i] + sum;
       }
     sum = sum/array.length;
     return sum;
-    }
   }
